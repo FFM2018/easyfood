3c5889a5981b03f17aef5da4109c1c2d6b13f181
Felipe <felipe_fmachado@hotmail.com>
2022-03-01 10:01:29 -0300

    primeiro commit

---
+++ src/test/java/br/com/api/junit/EasyfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
